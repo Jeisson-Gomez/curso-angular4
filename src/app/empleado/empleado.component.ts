bd39f3c397a8b5d5bb3992dcaa806f24a5a0a3e2
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-10-21 17:54:30 -0500

    Actualizacion del proyecto

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
     selector: 'empleado-tag',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 
 export class EmpleadoComponent{
     public titulo = 'Titulo del Componente Empleado: ';
+
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
+
+    constructor(){
+        this.empleado = new Empleado ('David Lopez', 45, 'Cocinero', true);
+        this.trabajadores = [
+            new Empleado ('Manolo Martinsez', 35, 'Administrador', true),
+            new Empleado ('Ana Lopez', 25, 'Cocinera', true),
+            new Empleado ('Jeisson Gomez', 66, 'Programador', false)
+        ];
+    }
+
+    ngOnInit(){
+        console.log(this.empleado);
+    }
 }
