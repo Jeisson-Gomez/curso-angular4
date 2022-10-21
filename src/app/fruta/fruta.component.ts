df69f8f849d166843951395f47879de709cdc025
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-10-21 17:02:30 -0500

    Actualizacion del proyecto

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -9,11 +9,44 @@ export class FrutaComponent{
     public nombre_componente = 'Componente de fruta';
     public listado_fruta = 'Naranja, Mandarina, Banano, Sandia';
 
-    public nombre:string = 'Jeisson Gomez';
-    public edad:number = 33;
-    public mayorDeEdad:boolean = true;
-    public trabajadore:Array<any> = ['Ingeniero', 44, 'Programador'];
-    comodin:any = 'Si'
+    public nombre:string;
+    public edad:number;
+    public mayorDeEdad:boolean;
+    public trabajos:Array<any> = ['Ingeniero', 44, 'Programador'];
+    comodin:any;
 
-    constructor(){}   
+    constructor(){
+        this.nombre = 'Jeisson Gomez';
+        this.edad = 33;
+        this.mayorDeEdad = true;
+        this.comodin = "Si"
+    }
+
+    ngOnInit(){
+        this.cambiarNombre();
+        this.cambiarEdad(55);
+        console.log(this.nombre + " " + this.edad);
+
+        //Variables y alcance
+        var uno = 8;
+        var dos = 15;
+
+        if(uno == 8){
+            let uno = 3;
+            var dos = 88;
+
+            console.log("Dentro del If: " + uno + " " + dos);
+        }
+
+        console.log("Dentro del If: " + uno + " " + dos);
+
+    }
+
+    cambiarNombre(){
+        this.nombre = 'Jey Jey';
+    }
+
+    cambiarEdad(edad: number){
+        this.edad = edad;
+    }
 }
