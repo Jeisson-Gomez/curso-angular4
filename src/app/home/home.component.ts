869550d65f9a36690137c99bc4595c89afb0f5a2
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-10-31 15:56:44 -0500

    Actualizacion del proyecto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,9 +12,13 @@ export class HomeComponent{
     public listado_ropa!:Array<string>;
     public prenda_a_guardar!:string;
 
+    public fecha;
+
     constructor(
         private _ropaService: RopaService
-    ){}
+    ){
+        this.fecha = new Date(2022, 9, 31);
+    }
 
     ngOnInit(){
         this.listado_ropa = this._ropaService.getRopa();
